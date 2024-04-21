eda4bacfa9ebe316b3e27e584636660461833341
maria-shpatserman <sokunova@gmail.com>
2024-04-21 11:52:17 +0800

    section 2 part2

--- 02-sping-core/02-component-scanning/src/main/java/ru/netunix/springcoredemo/SpringcoredemoApplication.java
+++ 02-sping-core/02-component-scanning/src/main/java/ru/netunix/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,10 @@ package ru.netunix.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*@SpringBootApplication(
+		scanBasePackages = {"ru.netunix.springcoredemo",
+		"ru.netunix.util"}
+)*/
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
