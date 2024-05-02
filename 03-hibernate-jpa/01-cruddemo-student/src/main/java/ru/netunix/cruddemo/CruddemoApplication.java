b65845da4af78a287941224f14054dd6cd91fecf
maria-shpatserman <sokunova@gmail.com>
2024-05-02 21:46:41 +0800

    CRUD operations - READ

--- 03-hibernate-jpa/01-cruddemo-student/src/main/java/ru/netunix/cruddemo/CruddemoApplication.java
+++ 03-hibernate-jpa/01-cruddemo-student/src/main/java/ru/netunix/cruddemo/CruddemoApplication.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Bean;
 import ru.netunix.cruddemo.dao.StudentDAO;
 import ru.netunix.cruddemo.entity.Student;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -15,22 +18,66 @@ public class CruddemoApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
-		return r->{
-			createStudent(studentDAO);
-		};
-	}
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return r -> {
+//			createStudent(studentDAO);
+//            createMultipleStudents(studentDAO);
+//            createAndRetriveStudent(studentDAO);
+//            getAllStudents(studentDAO);
+            getStudentsByLastName(studentDAO);
+        };
+    }
+
+    private void getStudentsByLastName(StudentDAO studentDAO) {
+        List<Student> listStudents = studentDAO.findByLastName("Long");
+        listStudents.forEach(System.out::println);
+    }
+
+    private void getAllStudents(StudentDAO studentDAO) {
+        System.out.println("Get All Students...");
+        List<Student> students = studentDAO.findAll();
+        students.forEach(System.out::println);
+//        students
+//                .stream().filter(s->!s.getLastName().equals("Vong"))
+//                .peek(s->System.out.println("hello "+s)).count();
+//        List<Student> sortedStudents = students.stream()
+//                .sorted((p1, p2) -> p1.getLastName().compareTo(p2.getLastName()))
+//                .collect(Collectors.toList());
+//        sortedStudents.forEach(System.out::println);
+
+
+    }
+
+    private void createAndRetriveStudent(StudentDAO studentDAO) {
+        Student studentLong = new Student("MiShu", "Long", "long.mishu@email.com");
+        studentDAO.save(studentLong);
+        System.out.println("Student Long " + studentLong);
+        Student persistStudent = studentDAO.findById(studentLong.getId());
+        System.out.println("Persist student " + persistStudent);
+
+    }
+
+    private void createMultipleStudents(StudentDAO studentDAO) {
+        Student studentLong = new Student("HuanHuan", "Si", "si@email.com");
+        Student studentShi = new Student("MiSha", "Shi", "shi.misha@email.com");
+        Student studentYang = new Student("ArteMi", "Long", "long.atemi@email.com");
+        studentDAO.save(studentLong);
+        studentDAO.save(studentShi);
+        studentDAO.save(studentYang);
+
+
+    }
 
     private void createStudent(StudentDAO studentDAO) {
         //create student
-        Student newStudent = new Student("Ben", "Yang","yang@email.com" );
+        Student newStudent = new Student("Ben", "Yang", "yang@email.com");
 
         //persist student
-        System.out.println("Persist student "+ newStudent);
+        System.out.println("Persist student " + newStudent);
         studentDAO.save(newStudent);
 
         //get id of saved object
-        System.out.println("Get generated id "+ newStudent.getId());
+        System.out.println("Get generated id " + newStudent.getId());
 
     }
 }
--- 03-hibernate-jpa/01-cruddemo-student/src/main/java/ru/netunix/cruddemo/dao/StudentDAOImpl.java
+++ 03-hibernate-jpa/01-cruddemo-student/src/main/java/ru/netunix/cruddemo/dao/StudentDAOImpl.java
@@ -1,10 +1,14 @@
 package ru.netunix.cruddemo.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.netunix.cruddemo.entity.Student;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO {
@@ -21,5 +25,25 @@ public class StudentDAOImpl implements StudentDAO {
     @Transactional
     public void save(Student theStudent) {
         entityManager.persist(theStudent);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student", Student.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        TypedQuery<Student> query = entityManager
+                .createQuery("FROM Student WHERE lastName=:theData", Student.class);
+        query.setParameter("theData", lastName);
+
+        return query.getResultList();
     }
 }
