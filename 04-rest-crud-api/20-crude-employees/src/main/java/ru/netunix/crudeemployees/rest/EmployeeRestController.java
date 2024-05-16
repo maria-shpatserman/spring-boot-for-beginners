fe982fbf4b7f8d4f086882f47689eadcd8519983
maria-shpatserman <sokunova@gmail.com>
2024-05-16 13:55:32 +0800

    Section 4 - CRUD Service metod implementation +Exception Handler

--- 04-rest-crud-api/20-crude-employees/src/main/java/ru/netunix/crudeemployees/rest/EmployeeRestController.java
+++ 04-rest-crud-api/20-crude-employees/src/main/java/ru/netunix/crudeemployees/rest/EmployeeRestController.java
@@ -2,11 +2,13 @@ package ru.netunix.crudeemployees.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netunix.crudeemployees.entity.Employee;
 import ru.netunix.crudeemployees.service.EmployeeService;
 import ru.netunix.crudeemployees.service.EmployeeServiceImpl;
+import ru.netunix.crudeemployees.utils.EmployeeNotFoundException;
 
 import java.util.List;
 
@@ -23,5 +25,14 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee findById(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+        if( employee == null) {
+            throw  new EmployeeNotFoundException(String.format("Employee with id [%d] not found",employeeId));
+        }
+        return employee;
     }
 }
--- 04-rest-crud-api/20-crude-employees/src/main/java/ru/netunix/crudeemployees/service/EmployeeServiceImpl.java
+++ 04-rest-crud-api/20-crude-employees/src/main/java/ru/netunix/crudeemployees/service/EmployeeServiceImpl.java
@@ -1,5 +1,6 @@
 package ru.netunix.crudeemployees.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.netunix.crudeemployees.dao.EmployeeDAO;
@@ -10,12 +11,31 @@ import java.util.List;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     EmployeeDAO employeeDAO;
+
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO){
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
         this.employeeDAO = employeeDAO;
     }
+
     @Override
-    public List<Employee> findAll(){
+    public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
