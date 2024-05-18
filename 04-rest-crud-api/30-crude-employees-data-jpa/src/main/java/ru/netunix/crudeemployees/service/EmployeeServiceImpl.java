0b52be92c6cebee6b80dd68db464a8a1dfe5df35
maria-shpatserman <sokunova@gmail.com>
2024-05-18 19:31:14 +0800

    add spring data jpa project

---
+++ 04-rest-crud-api/30-crude-employees-data-jpa/src/main/java/ru/netunix/crudeemployees/service/EmployeeServiceImpl.java
@@ -0,0 +1,41 @@
+package ru.netunix.crudeemployees.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.netunix.crudeemployees.dao.EmployeeDAO;
+import ru.netunix.crudeemployees.entity.Employee;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
+    }
+}
