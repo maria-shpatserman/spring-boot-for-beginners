01099988836b4cdebfe20a44927abe92b67988a0
maria-shpatserman <sokunova@gmail.com>
2024-05-19 09:59:02 +0800

    Section 5 - CRUD using spring data rest automatically

--- 04-rest-crud-api/40-crude-employees-data-jpa-spring-rest/src/main/java/ru/netunix/crudeemployees/handler/EmployeeExceptionHandler.java
+++ 04-rest-crud-api/40-crude-employees-data-jpa-spring-rest/src/main/java/ru/netunix/crudeemployees/handler/EmployeeExceptionHandler.java
@@ -10,21 +10,22 @@ import ru.netunix.crudeemployees.utils.EmployeeNotFoundException;
 @ControllerAdvice
 public class EmployeeExceptionHandler {
     @ExceptionHandler
-    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exception){
+    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exception) {
         ;
         return new ResponseEntity<>(EmployeeErrorResponse.builder()
                 .message(exception.getMessage())
                 .status(HttpStatus.NOT_FOUND.value())
-                .timeStamp(System.currentTimeMillis()).build(),HttpStatus.NOT_FOUND);
+                .timeStamp(System.currentTimeMillis()).build(), HttpStatus.NOT_FOUND);
 
     }
+
     //add new exception handler
     @ExceptionHandler
-    public ResponseEntity<EmployeeErrorResponse> handleAllExceptions(Exception exception){
+    public ResponseEntity<EmployeeErrorResponse> handleAllExceptions(Exception exception) {
         return new ResponseEntity<>(EmployeeErrorResponse.builder()
                 .message(exception.getMessage())
                 .status(HttpStatus.BAD_REQUEST.value())
-                .timeStamp(System.currentTimeMillis()).build(),HttpStatus.BAD_REQUEST);
+                .timeStamp(System.currentTimeMillis()).build(), HttpStatus.BAD_REQUEST);
 
     }
 }
