26c98fdcde26707d2e785a394fe0e9986cb1d14b
maria-shpatserman <sokunova@gmail.com>
2024-06-28 08:21:59 +0800

    Section 6 MVC - fill list of countries from html hard code

--- 06-sping-mvc/01-thymeleaf-mvc-app/src/main/java/ru/netunix/spingboot/thymeleafapp/controller/StudentController.java
+++ 06-sping-mvc/01-thymeleaf-mvc-app/src/main/java/ru/netunix/spingboot/thymeleafapp/controller/StudentController.java
@@ -1,6 +1,7 @@
 package ru.netunix.spingboot.thymeleafapp.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +9,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.netunix.spingboot.thymeleafapp.model.Student;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @Controller
 @Slf4j
 public class StudentController {
+    @Value("${countries}")
+    List<String> countries;
+
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         Student student = new Student();
         theModel.addAttribute("student", student);
+        theModel.addAttribute("countries",countries);
 
         return "student-form";
 
@@ -24,7 +36,21 @@ public class StudentController {
     public String processStudentForm(@ModelAttribute("student") Student theStudent) {
 
         log.info("{}", theStudent);
+        String input = "Test   reversed words   frame";
 
+        log.info("input {}", input);
+        log.info("result {}", reverseWords(input));
         return "student-confirmation";
+    }
+
+    String reverseWords(String input) {
+        Set<String> keys = Set.copyOf(Arrays.asList(input.split(" ")));
+
+        String result = input;
+        for (String key : keys) {
+            StringBuilder reversed = new StringBuilder(key).reverse();
+            result = result.replace(key, reversed.toString());
+        }
+        return result;
     }
 }
--- 06-sping-mvc/01-thymeleaf-mvc-app/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Student.java
+++ 06-sping-mvc/01-thymeleaf-mvc-app/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Student.java
@@ -9,4 +9,5 @@ import lombok.NoArgsConstructor;
 public class Student {
     private String firstName;
     private String lastName;
+    private String country;
 }
