946091dff58a20f03e4644f885fb7cfb0d31f647
maria-shpatserman <sokunova@gmail.com>
2024-06-30 18:40:40 +0800

    Section 6 MVC -  MVC range validation rules to the field

--- 06-sping-mvc/02-thymeleaf-mvc-app-validation/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Customer.java
+++ 06-sping-mvc/02-thymeleaf-mvc-app-validation/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Customer.java
@@ -1,5 +1,7 @@
 package ru.netunix.spingboot.thymeleafapp.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -12,4 +14,7 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+    @Min(value = 0, message = "must be greater or equal to zero")
+    @Max(value = 10, message = "must be less or equal to 10")
+    private int freePasses;
 }
