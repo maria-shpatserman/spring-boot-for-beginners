05e762d10ee008a3ee63d78aae60fc3012d9ea06
maria-shpatserman <sokunova@gmail.com>
2024-07-01 21:19:42 +0800

    Section 6 MVC -  MVC add validation with custom message for standard exception of mismatch

--- 06-sping-mvc/02-thymeleaf-mvc-app-validation/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Customer.java
+++ 06-sping-mvc/02-thymeleaf-mvc-app-validation/src/main/java/ru/netunix/spingboot/thymeleafapp/model/Customer.java
@@ -15,9 +15,10 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName;
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater or equal to zero")
     @Max(value = 10, message = "must be less or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
 }
