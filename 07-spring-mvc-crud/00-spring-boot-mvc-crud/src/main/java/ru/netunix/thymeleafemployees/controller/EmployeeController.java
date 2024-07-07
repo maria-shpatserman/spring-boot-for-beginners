cb87378859920c6f168809949b3e92eb68468368
maria-shpatserman <sokunova@gmail.com>
2024-07-07 16:46:31 +0800

    Section 7 - MVC CRUD - Update and Delete employee by MVC forms

--- 07-spring-mvc-crud/00-spring-boot-mvc-crud/src/main/java/ru/netunix/thymeleafemployees/controller/EmployeeController.java
+++ 07-spring-mvc-crud/00-spring-boot-mvc-crud/src/main/java/ru/netunix/thymeleafemployees/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.netunix.thymeleafemployees.entity.Employee;
 import ru.netunix.thymeleafemployees.service.EmployeeService;
 
@@ -36,12 +37,26 @@ public class EmployeeController {
         theModel.addAttribute("employee", employee);
         return  "employees/form-add-employee";
     }
+    @GetMapping("/showFormForUpdate")
+    public String updateEmployee(@RequestParam("employeeId") int id, Model theModel){
+        //get the employee using service from db
+        Employee employee = employeeService.findById(id);
+        //set employee into the model to pre-populate the form
+        theModel.addAttribute("employee",employee);
+        //send model to the form
+        return  "employees/form-add-employee";
+    }
     @PostMapping("/save")
     public String saveNewEmployee(@ModelAttribute("employee") Employee theEmployee){
         //save the employee
         employeeService.save(theEmployee);
         //use redirect to prevent duplicate submissions
 
+        return "redirect:/employees/list";
+    }
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int id){
+        employeeService.deleteById(id);
         return "redirect:/employees/list";
     }
 }
