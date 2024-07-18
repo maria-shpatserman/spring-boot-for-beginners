ff062b1b16329e4d3a40dff489a1b65b4b0a000f
maria-shpatserman <sokunova@gmail.com>
2024-07-18 23:17:32 +0800

    Section 9 - Advanced JPA

---
+++ 09-springboot-jpa-hibernate/crudhibernate/src/main/java/ru/netunix/crudhibernate/CrudhibernateApplication.java
@@ -0,0 +1,13 @@
+package ru.netunix.crudhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudhibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudhibernateApplication.class, args);
+	}
+
+}
