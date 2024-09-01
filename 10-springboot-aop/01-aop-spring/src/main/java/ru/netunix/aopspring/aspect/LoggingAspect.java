cbb87afa6789f4ec00bc67bd0127dfb49a4bd5df
maria-shpatserman <sokunova@gmail.com>
2024-09-01 14:23:01 +0800

    section 10 - aspect with any parameter for method

--- 10-springboot-aop/01-aop-spring/src/main/java/ru/netunix/aopspring/aspect/LoggingAspect.java
+++ 10-springboot-aop/01-aop-spring/src/main/java/ru/netunix/aopspring/aspect/LoggingAspect.java
@@ -15,7 +15,8 @@ public class LoggingAspect {
         System.out.println("=====>>>>> Executing @Before advice on addAccount()");
 
     }
-    @Before("execution(* add*(ru.netunix.aopspring.entity.Account,..))")
+    //any parameter
+    @Before("execution(* add*(..))")
     public void beforeAddAccountAdviceWithParam(){
         System.out.println("=====>>>>> Executing @Before advice on * add*(Account) - with parameter");
 
