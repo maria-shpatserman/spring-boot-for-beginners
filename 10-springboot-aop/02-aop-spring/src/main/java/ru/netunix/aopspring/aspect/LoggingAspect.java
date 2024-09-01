4c2970064c21a88e307fe62acc44c2a78aff5749
maria-shpatserman <sokunova@gmail.com>
2024-09-01 16:35:04 +0800

    section 10 - aspect create pointcut

--- 10-springboot-aop/02-aop-spring/src/main/java/ru/netunix/aopspring/aspect/LoggingAspect.java
+++ 10-springboot-aop/02-aop-spring/src/main/java/ru/netunix/aopspring/aspect/LoggingAspect.java
@@ -3,22 +3,22 @@ package ru.netunix.aopspring.aspect;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 @Slf4j
 public class LoggingAspect {
-    //strict by return type  strcit by packege name any class any method
-    @Before("execution(void ru.netunix.aopspring.dao.*.*())")
+    //apply to all method in all classes in DAO package
+    @Pointcut("execution(* ru.netunix.aopspring.dao.*.*(..))")
+    private void forDaoPackage(){
+
+    }
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice(){
         System.out.println("=====>>>>> Executing @Before advice on any method  no params in our package");
 
     }
-    //any parameter
-//    @Before("execution(* ru.netunix..add*(..))")
-//    public void beforeAddAccountAdviceWithParam(){
-//        System.out.println("=====>>>>> Executing @Before advice on * add*(Account) - with parameter");
-//
-//    }
+
 }
